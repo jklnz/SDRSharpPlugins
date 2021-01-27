886fcaf12fec4f0a1feabc1e0c57f5f38d4f2f9f
jklnz <jklnz>
2021-01-27 20:54:49 +1300

    Add support to auto set the frequency when starting after feature request

--- SDRSharp.AutoStartRadio/AutoStartRadioPlugin.cs
+++ SDRSharp.AutoStartRadio/AutoStartRadioPlugin.cs
@@ -8,6 +8,7 @@ namespace SDRSharp.AutoStartRadio
     {
         private const int DEFAULT_INTERVAL = 5000;
         private const string _displayName = "Auto Start Radio";
+        private long _frequency = 0;
         private ISharpControl _control;
                         
         private AutoStartPanel _guiControl;
@@ -26,6 +27,7 @@ namespace SDRSharp.AutoStartRadio
         {
             _guiControl = new AutoStartPanel();
             _control = control;
+            _frequency = Utils.GetLongSetting("asrfrequency", 0);
             Timer timer = new Timer();
             timer.Interval = DEFAULT_INTERVAL;
             timer.Tick += (sender, e) =>
@@ -47,11 +49,43 @@ namespace SDRSharp.AutoStartRadio
             try
             {
                 if (_control != null)
+                {
                     _control.StartRadio();
+                    // once we have started, check if we need to set frequency
+                    if (_frequency == 0)
+                        return;
+                    // set the tuning timer
+                    Timer timer = new Timer();
+                    timer.Interval = DEFAULT_INTERVAL;
+                    timer.Tick += (sender, e) =>
+                    {
+                        timer.Enabled = false;
+                        TuneRadio();
+                    };
+                    timer.Enabled = true;
+                }
             }
             catch (System.ApplicationException)
             {
                 MessageBox.Show("Could not start the radio, is there a device connected?", "Auto Start Radio");
+            }
+        }
+        /// <summary>
+        /// Attempts to tune the radio to a set frequency
+        /// </summary>
+        private void TuneRadio()
+        {
+            try
+            {
+                if (_control != null)
+                {
+                    
+                    _control.SetFrequency(_frequency, false);
+                }
+            }
+            catch (System.ApplicationException)
+            {
+                MessageBox.Show("Could not tune the radio", "Auto Start Radio");
             }
         }
     }
